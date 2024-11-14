0df8ecad4292ce08c761d8d17e4e9dc0cffa0c90
ggexzin <145757435+ggexzin@users.noreply.github.com>
2024-11-14 20:16:32 -0100

    Update Carro.cpp

--- Carro.cpp
+++ Carro.cpp
@@ -1,10 +1,12 @@
 #include "Carro.h"
 #include <iostream>
 
+using namespace std;
+
 Carro::Carro(const char* nome, int portas) : Veiculo(nome), numeroDePortas(portas) {}
 
 void Carro::mostrarDetalhes() const {
-    Veiculo::mostrarDetalhes();
+    cout << "Nome do Veículo: " << nome << endl;
     cout << "Tipo: Carro" << endl;
     cout << "Número de Portas: " << numeroDePortas << endl;
 }
