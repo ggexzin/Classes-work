e88be41881877016d8cd32d0c45d987189e95c72
ggexzin <145757435+ggexzin@users.noreply.github.com>
2024-11-14 20:17:50 -0100

    Update Mota.cpp

--- Mota.cpp
+++ Mota.cpp
@@ -1,11 +1,12 @@
-#include "Moto.h"
+#include "Mota.h"
 #include <iostream>
+
 using namespace std;
 
 Moto::Moto(const char* nome, const char* guidao) : Veiculo(nome), tipoDeGuidao(guidao) {}
 
 void Moto::mostrarDetalhes() const {
-    Veiculo::mostrarDetalhes();
+    cout << "Nome do Veículo: " << nome << endl;
     cout << "Tipo: Moto" << endl;
     cout << "Tipo de Guidão: " << tipoDeGuidao << endl;
 }
