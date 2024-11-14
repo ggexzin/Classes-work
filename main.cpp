7b9df28062edac52179de73dd78fa6f4e25057cb
ggexzin <145757435+ggexzin@users.noreply.github.com>
2024-11-14 21:16:06 -0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include "Carro.h"
-#include "Moto.h"
+#include "Mota.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    Carro carro("Toyota Corolla", 4);
-    Moto moto("Honda CG", "Esportivo");
+    Carro carro("Toyota GT86", 4, 130, "Preto Fosco", 20347.95, "Los Santos Customs", "Toyota", "FIXE-2024");
+    Mota mota("Honda Seila",300, "Branco brilhante", 6070.90, "6STR", "Honda", "FIXE-2025");
 
-    cout << "Detalhes do Carro:" << endl;
+
+    cout << "╓─────────────────────────────╖" << endl;
+    cout << "║            Carros 🏎️          ║" << endl;
+    cout << "╙──────────────────────────────╜" << endl;
     carro.mostrarDetalhes();
-
-    cout << "\nDetalhes da Moto:" << endl;
-    moto.mostrarDetalhes();
+    
+    cout << "┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉" << endl;
+    
+    cout << "╓─────────────────────────────╖" << endl;
+    cout << "║            Motas 🏍️          ║" << endl;
+    cout << "╙─────────────────────────────╜" << endl;
+    mota.mostrarDetalhes();
 
     return 0;
 }
