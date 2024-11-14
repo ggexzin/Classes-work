1d8d65859968a997749552b768b5aa7493244f22
ggexzin <145757435+ggexzin@users.noreply.github.com>
2024-11-14 21:14:50 -0100

    Update Carro.h

--- Carro.h
+++ Carro.h
@@ -2,13 +2,21 @@
 #define CARRO_H
 
 #include "Veiculo.h"
+#include <string>
 
 class Carro : public Veiculo {
 private:
     int numeroDePortas;
+    int cavalos;
+    string cor;
+    float preco;
+    string vendedor;
+    string marca;
+    string matricula;
 
 public:
-    Carro(const char* nome, int portas);
+    Carro(const char* nome, int portas, int cavalos, const string& cor, float preco, 
+          const string& vendedor, const string& marca, const string& matricula);
     void mostrarDetalhes() const;
 };
 
