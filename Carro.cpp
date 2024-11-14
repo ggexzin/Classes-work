73c35c2bd93edad96ecfa0470a99e9eba67cd022
ggexzin <145757435+ggexzin@users.noreply.github.com>
2024-11-14 21:14:32 -0100

    Update Carro.cpp

--- Carro.cpp
+++ Carro.cpp
@@ -3,10 +3,20 @@
 
 using namespace std;
 
-Carro::Carro(const char* nome, int portas) : Veiculo(nome), numeroDePortas(portas) {}
+Carro::Carro(const char* nome, int portas, int cavalos, const string& cor, float preco, 
+             const string& vendedor, const string& marca, const string& matricula)
+    : Veiculo(nome), numeroDePortas(portas), cavalos(cavalos), cor(cor), preco(preco), 
+      vendedor(vendedor), marca(marca), matricula(matricula) {}
 
 void Carro::mostrarDetalhes() const {
     cout << "Nome do Veículo: " << nome << endl;
     cout << "Tipo: Carro" << endl;
     cout << "Número de Portas: " << numeroDePortas << endl;
+    cout << "Cavalos: " << cavalos << endl;
+    cout << "Cor: " << cor << endl;
+    cout << "Preço: " << preco << endl;
+    cout << "Vendedor: " << vendedor << endl;
+    cout << "Marca: " << marca << endl;
+    cout << "Matrícula: " << matricula << endl;
+    cout << endl;
 }
