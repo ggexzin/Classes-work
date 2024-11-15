c04d883fcd7b6d4ac77160acd1ad3c2d29cc57f8
ggexzin <145757435+ggexzin@users.noreply.github.com>
2024-11-15 09:03:37 -0100

    Update Carro.h

--- Carro.h
+++ Carro.h
@@ -4,20 +4,22 @@
 #include "Veiculo.h"
 
 class Carro : public Veiculo {
-private:
-    int numeroDePortas;
-    int cavalos;
-    string cor;
-    float preco;
-    string vendedor;
-    string marca;
-    string matricula;
-
-public:
-    Carro(string nome, int portas, int cavalos, string cor, float preco, //recebe o q tem no veiculo e herda o resta das variaveis
-          string vendedor, string marca, string matricula) : Veiculo(nome), numeroDePortas(numeroDePortas), cavalos(cavalos), cor(cor), preco(preco), 
-        vendedor(vendedor), marca(marca), matricula(matricula){}
-    void mostrarDetalhes() override;
+    private:
+        //atributos do carro
+        int numeroDePortas;
+        int cavalos;
+        string cor;
+        float preco;
+        string vendedor;
+        string marca;
+        string matricula;
+    
+    public:
+        //construtor
+        Carro(string nome, int portas, int cavalos, string cor, float preco, //recebe o q tem no veiculo e herda o resta das variaveis
+              string vendedor, string marca, string matricula) : Veiculo(nome), numeroDePortas(numeroDePortas), cavalos(cavalos), cor(cor), preco(preco), 
+            vendedor(vendedor), marca(marca), matricula(matricula){}
+        void mostrarDetalhes() override;
 };
 
 #endif 
