e24822a05ca48b0b7ed45091470931715e605827
ggexzin <145757435+ggexzin@users.noreply.github.com>
2024-11-15 08:52:41 -0100

    Update Carro.h

--- Carro.h
+++ Carro.h
@@ -14,8 +14,9 @@ private:
     string matricula;
 
 public:
-    Carro(string nome, int portas, int cavalos, string cor, float preco, 
-          string vendedor, string marca, string matricula);
+    Carro(string nome, int portas, int cavalos, string cor, float preco, //recebe o q tem no veiculo e herda o resta das variaveis
+          string vendedor, string marca, string matricula) : Veiculo(nome), numeroDePortas(numeroDePortas), cavalos(cavalos), cor(cor), preco(preco), 
+        vendedor(vendedor), marca(marca), matricula(matricula){}
     void mostrarDetalhes() override;
 };
 
