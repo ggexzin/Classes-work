ce742e1d82b472b3af9fb5354546da4c04e33faf
ggexzin <145757435+ggexzin@users.noreply.github.com>
2024-11-15 09:01:33 -0100

    Update Mota.h

--- Mota.h
+++ Mota.h
@@ -13,7 +13,7 @@ private:
     string matricula;
 
 public:
-    Mota(string nome, int celindrada, string cor, float preco, 
+    Mota(string nome, int celindrada, string cor, float preco, //recebe o q tem no veiculo e herda o resta das variaveis
          string vendedor, string marca, string matricula) : Veiculo(nome), celindrada(celindrada), cor(cor), preco(preco), 
         vendedor(vendedor), marca(marca), matricula(matricula){};
     void mostrarDetalhes() override;
