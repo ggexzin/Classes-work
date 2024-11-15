f153dca16b065795818b01184ad6912f7f5e754b
ggexzin <145757435+ggexzin@users.noreply.github.com>
2024-11-15 09:05:12 -0100

    Update Mota.h

--- Mota.h
+++ Mota.h
@@ -4,21 +4,21 @@
 #include "Veiculo.h"
 
 class Mota : public Veiculo {
-private:
-    //atributos da mota
-    int celindrada;
-    string cor;
-    float preco;
-    string vendedor;
-    string marca;
-    string matricula;
-
-public:
-    //construtor
-    Mota(string nome, int celindrada, string cor, float preco, //recebe o q tem no veiculo e herda o resta das variaveis
-         string vendedor, string marca, string matricula) : Veiculo(nome), celindrada(celindrada), cor(cor), preco(preco), 
-        vendedor(vendedor), marca(marca), matricula(matricula){};
-    void mostrarDetalhes() override;
+    private:
+        //atributos da mota
+        int celindrada;
+        string cor;
+        float preco;
+        string vendedor;
+        string marca;
+        string matricula;
+    
+    public:
+        //construtor
+        Mota(string nome, int celindrada, string cor, float preco, //recebe o q tem no veiculo e herda o resta das variaveis
+             string vendedor, string marca, string matricula) : Veiculo(nome), celindrada(celindrada), cor(cor), preco(preco), 
+             vendedor(vendedor), marca(marca), matricula(matricula){};
+        void mostrarDetalhes() override;
 };
 
 #endif 
