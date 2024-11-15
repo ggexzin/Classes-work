a6d92dfcf40e387b7efa2100e48598ec5fc1aa23
ggexzin <145757435+ggexzin@users.noreply.github.com>
2024-11-15 08:32:32 -0100

    Update Veiculo.h

--- Veiculo.h
+++ Veiculo.h
@@ -4,12 +4,12 @@
 using namespace std;
 
 class Veiculo {
-private:
-    const string nome;
+    private:
+        const string nome;
 
-public:
-    Veiculo(const string* nome);
-    void mostrarDetalhes();
+    public:
+        Veiculo(const string* nome);
+        void mostrarDetalhes();
 };
 
 #endif 
